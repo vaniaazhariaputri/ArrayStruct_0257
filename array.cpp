d3b1887315197de53679ec41fd7790b847eb1581
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-15 11:23:47 +0700

    menampilkan nama, alamat mahasiswa

--- array.cpp
+++ array.cpp
@@ -81,3 +81,17 @@ struct mahasiswa
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20220140102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
