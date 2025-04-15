bde1594a3d7eced6c96b332e27af915f45563b88
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-15 11:21:32 +0700

    memasukkan nama mahasiswa

--- array.cpp
+++ array.cpp
@@ -43,3 +43,19 @@ struct mahasiswa
     string nama;
     detailAlamat alamat;
 };
+
+int main(){
+    mahasiswa mhs[2];
+
+    for (int i = 0; i  < 2; i++){
+        cout << "Mahasiswa ke-"<< i+1 << endl;
+        cout << "Masukkan NIM = ";
+        cin >> mhs[i].nim;
+        cin.ignore();
+        cout << "Masukkan NAMA = ";
+        getline(cin , mhs[i].nama);
+        cout << "Masukkan Alamat Desa = ";
+        cin >> mhs[i].alamat.desa;
+        cout << "Masukkan Alamat Kota = ";
+        cin >> mhs[i].alamat.kota;
+    }
