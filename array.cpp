d0d7549ca7aa7fc2692dcb47fbf525c79e8d9451
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-15 11:14:52 +0700

    deklarasi awal

--- array.cpp
+++ array.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 //deklarasi array dengan ukuran, dan diiberi nilai awal
 int nilai[]={1,2,3};
+
+//deklarasi array dengan ukuran, dan diberi nilai awal
+int skor[4]={9,13,20,11};
+
+//deklarasi array dengan ukuran, tanpa nilai awal
+string nama[3];
