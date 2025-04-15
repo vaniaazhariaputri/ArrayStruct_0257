e1a61028735cf7dafc597b434706443c4bf3e3e8
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-15 11:26:04 +0700

    mengeluarkan data nim, nama, alamat mahasiswa

--- array.cpp
+++ array.cpp
@@ -112,3 +112,22 @@ struct detailAlamat
     string desa;
     string kota;
 };
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
