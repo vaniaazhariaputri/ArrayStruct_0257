5c3e24dc662dbeda538592b2dab0b60b5e03c1a5
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-15 11:17:03 +0700

    mengisi isi data skor yang ketiga

--- array.cpp
+++ array.cpp
@@ -9,3 +9,15 @@ int skor[4]={9,13,20,11};
 
 //deklarasi array dengan ukuran, tanpa nilai awal
 string nama[3];
+
+int main(){
+    cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+    //Mengisi array pada index tertentu secara langsung
+    skor[2] = 50;
+    cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+
+    //Mengisi array dengan looping for
+    for(int i = 0; i < 3; i++){
+        cout << "Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
